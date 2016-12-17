38da5d28008763e09a070d97d4a71be8ce6f3dc2
jordan <j.schatzman@outlook.com>
2016-12-17 12:45:32 -0800

    adding test txt html and image files

---
+++ src/webroot/sample.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+"""
+myname.py.  Create sample page that prints CodeFellows!
+"""
+
+
+html = """
+<http>
+<body>
+<h2> The time is: </h2>
+<p> CodeFellows <p>
+</body>
+</http>
+"""
+
+
+print(html)
