485f6a0da3fa2360b192fc717bd3816ff866c515
jordan <j.schatzman@outlook.com>
2016-12-18 16:01:10 -0800

    updating setup

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup
 
 setup(
-    name='Server Part 3',
+    name='Server Concurrency',
     description='Implementation of locally hosted server and client.',
     version=0.1,
     author='Jordan Schatzman, Regenal Grant',
     author_email='j.schatzman@outlook.com',
     license='MIT',
     package_dir={'': 'src'},
-    py_modules=['echo_server'],
+    py_modules=['server', 'client'],
+    install_requires=['gevent'],
     extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox']},
 )
