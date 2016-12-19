2fd496ef77e3d1427153f4940b225eeb7d966ce1
jordan <j.schatzman@outlook.com>
2016-12-18 19:36:06 -0800

    change client so it can take in parameter request or use default

--- src/client.py
+++ src/client.py
@@ -40,6 +40,9 @@ def echo_message(client, message, buffer_length):
 
 
 if __name__ == '__main__':
-    request = 'GOT requestfiles/sample.html HTTP/1.1\r\n'
+    request = 'GET requestfiles/sample.html HTTP/1.1\r\n'
     request += 'host: http://www.example.com\r\n\r\n'
-    client(request)
+    if not sys.argv[1]:
+        client(request)
+    else:
+        client(sys.argv[1])
